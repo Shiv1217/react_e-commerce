da73ddde0813cb0b50875741806eb0c474239f8d
Shiv1217 <guptashivnarayan017@gmail.com>
2021-06-28 14:10:12 +0530

    Updates

---
+++ precache-manifest.208d5a8f9d4a39aef533f67197616ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7132f818e11814640114657d5b5b58",
+    "url": "/react_e-commerce/index.html"
+  },
+  {
+    "revision": "67a51c79b49341ef7d99",
+    "url": "/react_e-commerce/static/js/2.8a3593f0.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/react_e-commerce/static/js/2.8a3593f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344a0701551dce0fe183",
+    "url": "/react_e-commerce/static/js/main.74690f10.chunk.js"
+  },
+  {
+    "revision": "01dabb5d3a1dac7ebef0",
+    "url": "/react_e-commerce/static/js/runtime-main.c9acae47.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/react_e-commerce/static/media/commerce.422fec10.png"
+  }
+]);
